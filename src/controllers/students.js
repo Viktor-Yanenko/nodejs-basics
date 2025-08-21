45349eef65e74eecd698175b7fcfa9d07c0e5628
Viktor <viktoryanenko11@gmail.com>
2025-08-21 14:44:16 +0300

    add cloudinary photo upload

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -10,6 +10,8 @@ import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
 import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { getEnvVar } from '../utils/getEnvVar.js';
 
 export const getStudentsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
@@ -88,8 +90,13 @@ export const patchStudentController = async (req, res, next) => {
   let photoUrl;
 
   if (photo) {
+    if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    }
+  } else {
     photoUrl = await saveFileToUploadDir(photo);
   }
+
   const result = await updateStudent(studentId, {
     ...req.body,
     photo: photoUrl,
